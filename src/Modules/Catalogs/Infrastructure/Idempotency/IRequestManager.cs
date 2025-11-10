ce1d01dcc971ec79b38de0a1e41244ed2d5e04f1
mohammed khairelseed <mkhairelseed@outlook.com>
2025-11-10 12:28:39 +0300

    refactor: remove IRequestManager interface as it is no longer needed

--- src/Modules/Catalogs/Infrastructure/Idempotency/IRequestManager.cs
+++
@@ -1,7 +0,0 @@
-namespace Hababk.Modules.Catalogs.Infrastructure.Idempotency; 
-
-public interface IRequestManager
-{
-    Task<bool> IsExistAsync(Guid id);
-    Task CreateRequestForCommandAsync<T>(Guid id);
-}
